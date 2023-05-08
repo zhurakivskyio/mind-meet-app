aea3c3fdb0ee81b21e63d9a2335159e660bbbaf6
zhurakivskyio <zhurakivskyi.o@gmail.com>
2023-05-08 17:56:27 +0200

    Initial commit

---
+++ src/test/java/com/example/mindmeetapp/MindMeetAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mindmeetapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MindMeetAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
